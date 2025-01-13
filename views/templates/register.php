385bf9d148160a0f997584894e855152d15ef4af
Gurbala Laurent <69318652+laurentGurbala@users.noreply.github.com>
2025-01-13 21:19:01 +0100

    ajout de la page inscription #4
    - création de la vue register.php
    - ajout du dossier et image de la page
    - création/modification des fichiers css

--- views/templates/register.php
+++ views/templates/register.php
@@ -5,4 +5,33 @@
  */
 
 ?>
-register
+
+<div class="container">
+    <div class="form-container">
+        <h1>Inscription</h1>
+        <form action="index.php?action=registerUser" method="post">
+            <!-- Pseudo -->
+            <div class="form-group">
+                <label for="login">Pseudo</label>
+                <input type="text" id="login" name="login" required>
+            </div>
+            <!-- Email -->
+            <div class="form-group">
+                <label for="email">Adresse email</label>
+                <input type="email" id="email" name="email" required>
+            </div>
+            <!-- Mot de passe -->
+            <div class="form-group">
+                <label for="password">Mot de passe</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <button class="btn btn-primary btn-large" type="submit">S'inscrire</button>
+        </form>
+
+        <!-- Déjà inscrit -->
+        <div class="link">
+            <p>Déjà inscrit ? <a href="#">Connectez-vous</a></p>
+        </div>
+    </div>
+    <div class="image-container" aria-hidden="true"></div>
+</div>
